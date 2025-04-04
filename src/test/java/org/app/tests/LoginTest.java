29b9aa7f82c50598431fc19e0734d2d06a2331b6
ManalIELD <immanalib@gmail.com>
2025-04-04 23:27:52 +0200

    allure updated v2.0

--- src/test/java/org/app/tests/LoginTest.java
+++ src/test/java/org/app/tests/LoginTest.java
@@ -4,6 +4,8 @@ import io.appium.java_client.android.AndroidDriver;
 import org.app.pages.CartPage;
 import org.app.pages.LoginPage;
 import org.app.pages.ProductsPage;
+import org.app.utils.AllureUtil;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
 public class LoginTest extends TestBase {
@@ -30,5 +32,9 @@ public class LoginTest extends TestBase {
         //cartPage.validateTotalPurchaseAmount();
         cartPage.selectEmailCheckbox();
         cartPage.clickPurchaseButton();
+    }
+    @AfterClass
+    public void afterClass(){
+        AllureUtil.attachLogsToAllureReport();
     }
 }
